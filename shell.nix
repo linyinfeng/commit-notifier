c7544d340c2024570a533a50e9379b9ab169bd34
Lin Yinfeng <lin.yinfeng@outlook.com>
2021-10-08 12:51:15 +0800

    Move nix stuffs to nur-packages

---
+++ shell.nix
@@ -0,0 +1,15 @@
+{ pkgs ? import <nixpkgs> { } }:
+
+pkgs.mkShell {
+  packages = with pkgs; [
+    rustc
+    cargo
+
+    pkg-config
+    sqlite
+    libgit2
+    openssl
+  ];
+
+  RUST_LOG = "info,commit-notifier=debug";
+}
