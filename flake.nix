af857cc95394520bd6c6dbd1d9336ddd940787f6
Lin Yinfeng <lin.yinfeng@outlook.com>
2023-11-08 13:10:43 +0800

    Initialize nix flake

---
+++ flake.nix
@@ -0,0 +1,99 @@
+{
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs";
+
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    flake-utils.url = "github:numtide/flake-utils";
+    flake-utils.inputs.systems.follows = "systems";
+
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+    treefmt-nix.inputs.nixpkgs.follows = "nixpkgs";
+
+    crane.url = "github:ipetkov/crane";
+    crane.inputs.nixpkgs.follows = "nixpkgs";
+
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    rust-overlay.inputs.flake-utils.follows = "flake-utils";
+    rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
+
+    systems.url = "github:nix-systems/default";
+  };
+
+  outputs = inputs @ {flake-parts, ...}:
+    flake-parts.lib.mkFlake {inherit inputs;}
+    ({
+      config,
+      self,
+      inputs,
+      lib,
+      getSystem,
+      ...
+    }: {
+      systems = import inputs.systems;
+      imports = [
+        inputs.flake-parts.flakeModules.easyOverlay
+        inputs.treefmt-nix.flakeModule
+      ];
+      flake = {
+        nixosModules.commit-notifier = ./nixos/commit-notifier.nix;
+      };
+      perSystem = {
+        config,
+        self',
+        pkgs,
+        system,
+        ...
+      }: let
+        craneLib = inputs.crane.lib.${system};
+        src = craneLib.cleanCargoSource (craneLib.path ./.);
+        bareCommonArgs = {
+          inherit src;
+          nativeBuildInputs = with pkgs; [
+            pkg-config
+          ];
+          buildInputs = with pkgs; [
+            sqlite
+            libgit2
+            openssl
+          ];
+        };
+        cargoArtifacts = craneLib.buildDepsOnly bareCommonArgs;
+        commonArgs = bareCommonArgs // {inherit cargoArtifacts;};
+      in {
+        packages = {
+          commit-notifier = craneLib.buildPackage commonArgs;
+          default = config.packages.commit-notifier;
+        };
+        overlayAttrs = {
+          inherit (config.packages) commit-notifier;
+        };
+        checks = {
+          inherit (self'.packages) commit-notifier;
+          doc = craneLib.cargoDoc commonArgs;
+          fmt = craneLib.cargoFmt {inherit src;};
+          nextest = craneLib.cargoNextest commonArgs;
+          clippy = craneLib.cargoClippy (commonArgs
+            // {
+              cargoClippyExtraArgs = "--all-targets -- --deny warnings";
+            });
+        };
+        treefmt = {
+          projectRootFile = "flake.nix";
+          programs = {
+            alejandra.enable = true;
+            rustfmt.enable = true;
+            shfmt.enable = true;
+          };
+        };
+        devShells.default = pkgs.mkShell {
+          inputsFrom = lib.attrValues self'.checks;
+          packages = with pkgs; [
+            rustup
+            rust-analyzer
+          ];
+        };
+      };
+    });
+}
